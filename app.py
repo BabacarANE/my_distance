8326d832c1326cc7f09b19aab38ebe1d5d7f5862
BabacarANE <babacarane@gmail.com>
2026-06-15 16:49:47 +0200

    refactor: renommage des fonctions et variables selon PEP8 (snake_case)

--- app.py
+++ app.py
@@ -6,60 +6,54 @@ app = Flask('my_distance')
 
 distances = list()
 
+
 @app.route('/', methods=['GET', 'POST'])
-def html_calculate():
+def distance_form():
     if request.method == 'GET':
-    # Si get, afficher la page vide
         return render_template('index.html', result=None)
     if request.method == 'POST':
-    # Si post, calculer et afficher le résultat
-        eNd = tuple(map(lambda x: int(x), request.form['apoint'].split(',')[0:2]))
-        start = list(map(lambda y: int(y), request.form['bpoint'].split(',')[0:2]))
-        startPoint = start
-        result_tmp = sqrt((eNd[1] - start[1])**2 + (eNd[0] - startPoint[0])**2)
-        EndPoint = eNd
-        result =             {
-                    'requested_at': datetime.now(),
-                    'result_distance': result_tmp,
-                    'start_point': startPoint,
-                    'end_point': EndPoint
-                }
-        distances.append({
-                    'requested_at': datetime.now(),
-                    'result_distance': result_tmp,
-                    'start_point': startPoint,
-                    'end_point': EndPoint
-                })    
+        start_point = tuple(map(lambda x: int(x), request.form['apoint'].split(',')[0:2]))
+        end_point = tuple(map(lambda y: int(y), request.form['bpoint'].split(',')[0:2]))
+        result_tmp = sqrt((end_point[1] - start_point[1])**2 + (end_point[0] - start_point[0])**2)
+        result = {
+            'requested_at': datetime.now(),
+            'result_distance': result_tmp,
+            'start_point': start_point,
+            'end_point': end_point
+        }
+        distances.append(result)
         return render_template('index.html', result=result)
+
 
 @app.route('/api')
 def index():
     return {}
 
+
 @app.route('/api/distances')
-def already_calculated():
+def get_distances():
     result = list(map(lambda x: {
-                    'requested_at': x['requested_at'],
-                    'result_distance': x['result_distance'],
-                    'start_point': x['start_point'],
-                    'end_point': x['end_point']        
+        'requested_at': x['requested_at'],
+        'result_distance': x['result_distance'],
+        'start_point': x['start_point'],
+        'end_point': x['end_point']
     }, distances))
     return result
 
+
 @app.route('/api/distance', methods=['POST'])
-def Calculate():
-    startPoint = request.json['start_point']
-    startPoint = list(map(lambda y: int(y), request.json['start_point'].split(',')[0:2]))
-    EndPoint = tuple(map(lambda x: int(x), request.json['end_point'].split(',')[0:2]))
-    
-    result_tmp = sqrt((EndPoint[1] - startPoint[1])**2 + (EndPoint[0] - startPoint[0])**2)
-    result =             {
-                'requested_at': datetime.now(),
-                'result_distance': result_tmp,
-                'start_point': startPoint,
-                'end_point': EndPoint
-            }
+def calculate():
+    start_point = tuple(map(lambda y: int(y), request.json['start_point'].split(',')[0:2]))
+    end_point = tuple(map(lambda x: int(x), request.json['end_point'].split(',')[0:2]))
+    result_tmp = sqrt((end_point[1] - start_point[1])**2 + (end_point[0] - start_point[0])**2)
+    result = {
+        'requested_at': datetime.now(),
+        'result_distance': result_tmp,
+        'start_point': start_point,
+        'end_point': end_point
+    }
     return result
+
 
 if __name__ == '__main__':
     app.run(debug=True)
